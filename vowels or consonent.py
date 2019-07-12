1b79a72fc077f7e29bac29dbcba591cc40549f6a
VenkateshMohan98 <51222591+VenkateshMohan98@users.noreply.github.com>
2019-07-12 15:57:25 +1000

    Create vowels or consonent.py

---
+++ vowels or consonent.py
@@ -0,0 +1,9 @@
+v = input
+aph =['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v''w','x','y','z'] 
+vowels=['a','e','i','o','u']
+if v in vowels:
+  print("Vowels")
+elif v in aph:
+  print("Consonant")
+else:
+  print("invalid")
